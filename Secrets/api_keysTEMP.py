bb48f7e44d99a8a80b6f094910b2e999b34ca60d
cashhollister2u <hollisterUser333@gmail.com>
2024-07-06 21:31:38 -0400

    api_key template

---
+++ Secrets/api_keysTEMP.py
@@ -0,0 +1,10 @@
+# [spotify inputs] "developer.spotify.com"
+CLIENT_ID = "" # client id for spotify dev app
+CLIENT_SECRET = "" # client secret for spotify dev app
+VOLUME = 75 # Irrelevant
+
+# [stock market inputs] "twelvedata.com"
+TWELVE_API_KEY = ""
+
+# [clock inputs] "openweathermap.org"
+WEATHER_API_KEY = ""
