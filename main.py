41bf47aa5b34dc12dc514d7bf3b4e401803df965
cashhollister2u <153677541+cashhollister2u@users.noreply.github.com>
2024-07-17 15:02:50 -0400

    added thread for push button controller

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import time
 
 from Main_Components.display_select import display_loop
 from Main_Components.http_server import run_flask_server, user, shared_user
+from Main_Components.push_button import button_channel_change
 
 if __name__ == "__main__":
      # run flask app in threaded instance
      thread = threading.Thread(target=run_flask_server)
      thread.daemon = True
      thread.start()
+
+     button_controller = threading.Thread(target=button_channel_change, args=(user, shared_user))
+     button_controller.daemon = True
+     button_controller.start()
      
      time.sleep(1) # a bandaid for the race condition between flask and display_loop
 
