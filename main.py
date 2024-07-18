39543ed5adc1c4c4d7eebfaba2c81b0f0868b83a
“Cash <cashhollistergame@gmail.com>
2024-07-18 01:59:05 -0400

    stop the loading screen when the programstarts

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import threading
 import time
+import subprocess
 
 from Main_Components.display_select import display_loop
 from Main_Components.http_server import run_flask_server, shared_user
 from Main_Components.push_button import button_channel_change
+
+def stop_loading_service():
+    try:
+        result = subprocess.run(['sudo', 'systemctl', 'stop', 'loading_display.service'], check=True, text=True, capture_output=True)
+        print("Output:", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Error:", e.stderr)
 
 if __name__ == "__main__":
      # run flask app in threaded instance
@@ -14,6 +22,8 @@ if __name__ == "__main__":
      button_controller = threading.Thread(target=button_channel_change, args=(shared_user,))
      button_controller.daemon = True
      button_controller.start()
+     
+     stop_loading_service()
      
      time.sleep(1) # a bandaid for the race condition between flask and display_loop
 
